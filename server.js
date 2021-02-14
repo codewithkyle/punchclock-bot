d4ac7107bdc9c135b19133b57ac0c3632cb1bc29
Kyle Andrews <codingwithkyle@gmail.com>
2021-02-13 19:17:39 -0500

    Removed timezone plugin

--- server.js
+++ server.js
@@ -11,13 +11,8 @@ const path = require("path");
 const cwd = process.cwd();
 
 const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone');
 const customParseFormat = require('dayjs/plugin/customParseFormat')
-dayjs.extend(utc);
-dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
-dayjs.tz.setDefault("America/New_York");
 
 const auth = require('basic-auth');
 require('dotenv').config();
